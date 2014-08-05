c339ca43697b0d1398c6858e0a27d157166fa7d2
Adrian Maniatis <kwyjii@gmail.com>
2014-08-05 14:38:57 +1000

    Check in skeleton...

---
+++ app/js/controllers.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('bgstatsClient.controllers', [])
+  .controller('LoginCtrl', ['$scope', function($scope) {
+
+  }])
+  .controller('LatestCtrl', ['$scope', function($scope) {
+
+  }])
+  .controller('StatsCtrl', ['$scope', function($scope) {
+
+  }])
+  .controller('SubmitCtrl', ['$scope', function($scope) {
+
+  }]);
